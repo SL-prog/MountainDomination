6fd97b3e025b32ad3fada7ced517dc55e67a3ffb
regisseur <tomalector.97@orange.fr>
2015-02-09 17:45:11 +0100

    ajout d'images
    ... pour l'interface

--- constantes.py
+++ constantes.py
@@ -1,17 +1,21 @@
 #-------------------------------------------------------------------------------
 # Name:        constantes
 #
-# Author:      sl-prog
+# Author:      sl-prog et regisseur
 #
 # Licence:     <GNU GENERAL PUBLIC LICENSE>
 #-------------------------------------------------------------------------------
 
+import pygame
+from pygame.locals import *
+
 #Dimension ecran
 largeur_ecran = 640
-hauteur_ecran = 480
+hauteur_ecran = 560
 
 #Titre ecran
 titre_fenetre = 'Mountain Domination'
+fenetre = pygame.display.set_mode((largeur_ecran, hauteur_ecran), RESIZABLE) #fenetre de 640*480
 
 #Images
 icon = "image/icon.png"
@@ -19,6 +23,16 @@ background = "image/background.png"
 mountain = "image/map-test3.png"
 escargot_rouge = "image/esc-red.png"
 escargot_bleu = "image/esc-blue.png"
+
+arme1 = pygame.image.load("image/bazooka.png").convert()
+arme2 = pygame.image.load("image/grenade.png").convert_alpha()
+chargement0 = pygame.image.load("image/barre0.png").convert_alpha()
+chargement1 = pygame.image.load("image/barre1.png").convert_alpha()
+chargement2 = pygame.image.load("image/barre2.png").convert_alpha()
+chargement3 = pygame.image.load("image/barre3.png").convert_alpha()
+chargement4 = pygame.image.load("image/barre4.png").convert_alpha()
+horloge = pygame.image.load("image/horloge.png").convert_alpha()
+
 
 #vitesse et positions des personnages (TEST)
 vitesse_perso_x = 5
